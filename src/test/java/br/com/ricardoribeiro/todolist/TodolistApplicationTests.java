c3e6a7bdb3c382b1ab0174cea94620a431152a5b
Ricardo Ribeiro <ricardoribeiro.j7@gmail.com>
2023-10-10 21:41:45 -0300

    Project initialized.

---
+++ src/test/java/br/com/ricardoribeiro/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ricardoribeiro.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
